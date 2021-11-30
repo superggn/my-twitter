910c9d89fe5510c2a4a79e231ca2fd221086596c
superggn <939401399@qq.com>
2021-11-30 11:33:24 +0800

    fix-coding-format

--- twitter/urls.py
+++ twitter/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import debug_toolbar
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
@@ -31,8 +32,6 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    import debug_toolbar
-
     urlpatterns.append(
         path('__debug__', include(debug_toolbar.urls))
     )
