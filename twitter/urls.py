f8c0bc9c686ca605eeb3452d96151ed1b7baabef
superggn <939401399@qq.com>
2022-01-02 23:15:30 +0800

    add comments create api

--- twitter/urls.py
+++ twitter/urls.py
@@ -20,6 +20,7 @@ from django.urls import include, path
 from rest_framework import routers
 
 from accounts.api.views import AccountViewSet, UserViewSet
+from comments.api.views import CommentViewSet
 from friendships.api.views import FriendshipViewSet
 from newsfeeds.api.views import NewsFeedViewSet
 from tweets.api.views import TweetViewSet
@@ -30,6 +31,7 @@ router.register(r'api/accounts', AccountViewSet, basename='accounts')
 router.register(r'api/tweets', TweetViewSet, basename='tweets')
 router.register(r'api/friendships', FriendshipViewSet, basename='friendships')
 router.register(r'api/newsfeeds', NewsFeedViewSet, basename='newsfeeds')
+router.register(r'api/comments', CommentViewSet, basename='comments')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
