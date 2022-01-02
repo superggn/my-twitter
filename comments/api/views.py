b574e646eeb58e134420898f4947dc12919311e9
superggn <939401399@qq.com>
2022-01-03 00:20:35 +0800

    add comments list api; add django-filter; add local settings(logging)

--- comments/api/views.py
+++ comments/api/views.py
@@ -18,9 +18,10 @@ class CommentViewSet(viewsets.GenericViewSet):
     """
     serializer_class = CommentSerializerForCreate
     queryset = Comment.objects.all()
+    filterset_fields = ('tweet_id',)
 
     # POST /api/comments/ -> create(方法)
-    # GET /api/comments/ -> list(方法)
+    # GET /api/comments/?tweet_id=1 -> list(方法)
     # GET /api/comments/1/ -> retrieve(方法)
     # DELETE /api/comments/1/ -> destroy(方法)
     # PATCH /api/comments/1/ -> partial_update(方法)
@@ -34,6 +35,20 @@ class CommentViewSet(viewsets.GenericViewSet):
         if self.action in ['destroy', 'update']:
             return [IsAuthenticated(), IsObjectOwner()]
         return [AllowAny()]
+
+    def list(self, request, *args, **kwargs):
+        if 'tweet_id' not in request.query_params:
+            return Response({
+                'message': 'missing tweet_id in request',
+                'success': False,
+            }, status=status.HTTP_400_BAD_REQUEST)
+        queryset = self.get_queryset()
+        comments = self.filter_queryset(queryset).prefetch_related('user').order_by('created_at')
+        serializer = CommentSerializer(comments, many=True)
+        return Response(
+            {'comments': serializer.data},
+            status=status.HTTP_200_OK,
+        )
 
     def create(self, request, *args, **kwargs):
         data = {
--- twitter/settings.py
+++ twitter/settings.py
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 # Application definition
 
 INSTALLED_APPS = [
+    # django default
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     # third party
     'rest_framework',
     'debug_toolbar',
+    'django_filters',
     # project apps
     'accounts',
     'tweets',
@@ -48,7 +50,10 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ],
 }
 
 MIDDLEWARE = [
@@ -133,3 +138,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 INTERNAL_IPS = ['10.0.2.2']
+
+try:
+    from .local_settings import *
+except:
+    pass
