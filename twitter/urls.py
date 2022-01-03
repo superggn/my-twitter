48112e1668b37a0440ad8860edc964a5a592c43b
superggn <939401399@qq.com>
2022-01-03 13:03:47 +0800

    add likes create api

--- twitter/urls.py
+++ twitter/urls.py
@@ -22,6 +22,7 @@ from rest_framework import routers
 from accounts.api.views import AccountViewSet, UserViewSet
 from comments.api.views import CommentViewSet
 from friendships.api.views import FriendshipViewSet
+from likes.api.views import LikeViewSet
 from newsfeeds.api.views import NewsFeedViewSet
 from tweets.api.views import TweetViewSet
 
@@ -32,6 +33,7 @@ router.register(r'api/tweets', TweetViewSet, basename='tweets')
 router.register(r'api/friendships', FriendshipViewSet, basename='friendships')
 router.register(r'api/newsfeeds', NewsFeedViewSet, basename='newsfeeds')
 router.register(r'api/comments', CommentViewSet, basename='comments')
+router.register(r'api/likes', LikeViewSet, basename='likes')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
