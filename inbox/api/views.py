aa5a0b39c06a26a254eb67f37030654d945f861d
superggn <939401399@qq.com>
2022-01-12 23:16:19 +0800

    添加 notifications update api part 2

--- inbox/api/views.py
+++ inbox/api/views.py
@@ -4,7 +4,11 @@ from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
-from inbox.api.serializers import NotificationSerializer
+from inbox.api.serializers import (
+    NotificationSerializer,
+    NotificationSerializerForUpdate,
+)
+from utils.decorators import required_params
 
 
 class NotificationViewSet(
@@ -31,3 +35,33 @@ class NotificationViewSet(
         # queryset.update(): 批量操作
         updated_count = self.get_queryset().filter(unread=True).update(unread=False)
         return Response({'marked_count': updated_count}, status=status.HTTP_200_OK)
+
+    @required_params(method='PUT', params=['unread'])
+    def update(self, request, *args, **kwargs):
+        """
+        注意，这里的 update 对应的是 PUT 请求，PATCH 对应的是 PARTIAL UPDATE
+        用户可以标记一个 notification 为已读或者未读。标记已读和未读都是对 notification
+        的一次更新操作，所以直接重载 update 的方法来实现。另外一种实现方法是用一个专属的 action：
+            @action(methods=['POST'], detail=True, url_path='mark-as-read')
+            def mark_as_read(self, request, *args, **kwargs):
+                ...
+            @action(methods=['POST'], detail=True, url_path='mark-as-unread')
+            def mark_as_unread(self, request, *args, **kwargs):
+                ...
+        两种方法都可以，我更偏好重载 update，因为更通用更 rest 一些, 而且 mark as unread 和
+        mark as read 可以公用一套逻辑。
+        """
+        serializer = NotificationSerializerForUpdate(
+            instance=self.get_object(),
+            data=request.data,
+        )
+        if not serializer.is_valid():
+            return Response({
+                'message': "Please check input",
+                'errors': serializer.errors,
+            }, status=status.HTTP_400_BAD_REQUEST)
+        notification = serializer.save()
+        return Response(
+            NotificationSerializer(notification).data,
+            status=status.HTTP_200_OK,
+        )
