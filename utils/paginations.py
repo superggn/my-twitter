60fac5796317931cf7e513bef9c60995f0b37301
superggn <939401399@qq.com>
2022-02-16 09:13:33 +0800

    28-newsfeeds-pagination

--- utils/paginations.py
+++ utils/paginations.py
@@ -18,7 +18,7 @@ class EndlessPagination(BasePagination):
             # 为了简便起见，下拉刷新不做翻页机制，直接加载所有更新的数据
             # 因为如果数据很久没有更新的话，不会采用下拉刷新的方式进行更新，而是重新加载最新的数据
             created_at__gt = request.query_params['created_at__gt']
-            queryset = queryset.filter(id__gt=created_at__gt)
+            queryset = queryset.filter(created_at__gt=created_at__gt)
             self.has_next_page = False
             return queryset.order_by('-id')
 
@@ -29,7 +29,7 @@ class EndlessPagination(BasePagination):
             # 则应该返回 [9, 8, 7]，多返回一个 object 的原因是为了判断是否还有下一页
             # 从而减少一次空加载。
             created_at__lt = request.query_params['created_at__lt']
-            queryset = queryset.filter(id__lt=created_at__lt)
+            queryset = queryset.filter(created_at__lt=created_at__lt)
 
         queryset = queryset.order_by('-id')[:self.page_size + 1]
         self.has_next_page = len(queryset) > self.page_size
