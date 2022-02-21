419d334bc727984f3b90844612dfc50b4a086317
superggn <939401399@qq.com>
2022-02-21 10:02:16 +0800

    31-tweet-cache

--- newsfeeds/models.py
+++ newsfeeds/models.py
@@ -2,11 +2,13 @@ from django.contrib.auth.models import User
 from django.db import models
 
 from tweets.models import Tweet
+from utils.memcached_helper import MemcachedHelper
 
 
 class NewsFeed(models.Model):
     # 注意这个 user 不是存储谁发了这条 tweet，而是谁可以看到这条 tweet
     # 我发了一个帖子，我有3个粉丝，就创建3条 newsfeed 记录
+    # 这里的 user 不用 cache, 因为这是我们查询数据的时候用的，不是前端渲染使用
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     tweet = models.ForeignKey(Tweet, on_delete=models.SET_NULL, null=True)
     # 因为要加上一个排序，顺手加上一个
@@ -19,3 +21,7 @@ class NewsFeed(models.Model):
 
     def __str__(self):
         return f'{self.created_at} inbox of {self.user}: {self.tweet}'
+
+    @property
+    def cached_tweet(self):
+        return MemcachedHelper.get_object_through_cache(Tweet, self.tweet_id)
--- tweets/models.py
+++ tweets/models.py
@@ -1,10 +1,12 @@
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
+from django.db.models.signals import post_save, pre_delete
 
-from accounts.services import UserService
 from likes.models import Like
 from tweets.constants import TWEET_PHOTO_STATUS_CHOICES, TweetPhotoStatus
+from utils.listeners import invalidate_object_cache
+from utils.memcached_helper import MemcachedHelper
 from utils.time_helpers import utc_now
 
 
@@ -40,7 +42,7 @@ class Tweet(models.Model):
 
     @property
     def cached_user(self):
-        return UserService.get_user_through_cache(self.user_id)
+        return MemcachedHelper.get_object_through_cache(User, self.user_id)
 
 
 class TweetPhoto(models.Model):
@@ -83,4 +85,8 @@ class TweetPhoto(models.Model):
 
     @property
     def cached_user(self):
-        return UserService.get_user_through_cache(self.user_id)
+        return MemcachedHelper.get_object_through_cache(User, self.user_id)
+
+
+pre_delete.connect(invalidate_object_cache, sender=Tweet)
+post_save.connect(invalidate_object_cache, sender=Tweet)
