a6eb859bc5844fb83a25d87a5bebda09bef455a3
superggn <939401399@qq.com>
2022-03-03 10:04:00 +0800

    cache-tweet-list-in-redis

--- twitter/cache.py
+++ twitter/cache.py
@@ -2,3 +2,6 @@
 FOLLOWINGS_PATTERN = 'followings:{user_id}'
 # 注意，这里的 id 是 user_id, 不是 user_profile_id
 USER_PROFILE_PATTERN = 'userprofile:{user_id}'
+
+# redis
+USER_TWEETS_PATTERN = 'user_tweets:{user_id}'
